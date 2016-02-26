ccdc2c56c3ab9423f7a58c4e22e74700410b0f13
Merloni <tuomo.salmenkivi@gmail.com>
2016-02-26 16:09:51 +0200

    Nimimigraatio ja indexin säätöä. Tietokanta atm hajalla

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226131208) do
+ActiveRecord::Schema.define(version: 20160226135735) do
 
   create_table "guardianships", force: :cascade do |t|
     t.datetime "endtime"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160226131208) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end

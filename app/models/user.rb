55c59053a1139cc89d1e0c033e23a3d0ec8c5cc1
Felix Lindholm <felix.lindholm@gmail.com>
2016-03-01 23:50:59 +0200

    add devise authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   has_many  :guardianships
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
